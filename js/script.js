25db250ac35a2336588254389e7ad399d4fd8758
marypaduano <maria.paduano@gmail.com>
2023-02-22 20:10:10 +0100

    fix layout added toolbar msg

--- js/script.js
+++ js/script.js
@@ -7,6 +7,27 @@ createApp({
         return {
             contacts: [
                 {
+                    name: 'Luca',
+                    avatar: '',
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
                     name: 'Michele',
                     avatar: '',
                     messages: [
